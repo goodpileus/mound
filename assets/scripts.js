02f63f7cf9aa5e752c734922a72d2b01e0fcb890
Aidan Quinlan <aidankquinlan@gmail.com>
2022-07-26 13:23:48 -0700

    content test options

--- assets/scripts.js
+++ assets/scripts.js
@@ -126,3 +126,17 @@ $('a').click(function(){
     }, 200);
     return false;
 });
+
+function openTarget() {
+  var hash = location.hash.substring(1) + 'd';
+  console.log(hash);
+  if(hash) var details = document.getElementById(hash);
+  if(details && details.tagName.toLowerCase() === 'details') details.open = true;
+}
+window.addEventListener('load', openTarget);
+openTarget();
+
+
+$( "img" ).click(function() {
+  $( this ).toggleClass('active');
+});
